dfddcbf03d168da3f64586f65c9eb159d11ad315
yanuar <yanuarnauval.work@gmail.com>
2025-08-20 16:01:41 +0700

    style:[YN] create animation and search page

---
+++ lib/core/widgets/omdb_appbar_widget.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:omdbapp/core/widgets/widgets.dart';
+
+import '../../features/search/presentation/pages/search_delegate.dart';
+import '../gen/assets.gen.dart';
+import '../gen/colors.gen.dart';
+
+class OmdbAppbarWidget extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final bool centerTitle;
+  final Color backgroundColor;
+  final Map<String, dynamic> chipIconAndText;
+  final double bottomHeight;
+  final VoidCallback? onDownloadPressed;
+  final VoidCallback? onSearchPressed;
+  final ValueNotifier bodyScrollListener;
+
+  const OmdbAppbarWidget({
+    super.key,
+    this.title = "For Alex",
+    this.centerTitle = false,
+    this.backgroundColor = Colors.transparent,
+    required this.chipIconAndText,
+    this.bottomHeight = 48.0,
+    this.onDownloadPressed,
+    this.onSearchPressed,
+    required this.bodyScrollListener,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+      valueListenable: bodyScrollListener,
+      builder: (context, value, _) {
+        return AppBar(
+          title: Text(title),
+          centerTitle: centerTitle,
+          backgroundColor:  Color(0xFFD22F26).withValues(alpha: value),
+          bottom: PreferredSize(
+            preferredSize: Size.fromHeight(bottomHeight),
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Wrap(
+                children: chipIconAndText.entries
+                    .map((map) => OmdbChip(name: map.key, icon: map.value))
+                    .toList(),
+              ),
+            ),
+          ),
+          actions: [
+            IconButton(
+              onPressed: onDownloadPressed ?? () {},
+              icon: Assets.svg.downloadIcon.svg(
+                colorFilter: ColorFilter.mode(
+                  ColorValue.white,
+                  BlendMode.srcIn,
+                ),
+              ),
+            ),
+            IconButton(
+              onPressed:
+                  onSearchPressed ??
+                  () async {
+                    await showSearch(
+                      context: context,
+                      delegate: OmDbSearchDelegate(),
+                    );
+                  },
+              icon: Assets.svg.searchIcon.svg(
+                colorFilter: ColorFilter.mode(
+                  ColorValue.white,
+                  BlendMode.srcIn,
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight + bottomHeight);
+}
--- lib/features/home/presentation/pages/home_screen.dart
+++ lib/features/home/presentation/pages/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:omdbapp/core/gen/colors.gen.dart';
+import 'package:omdbapp/core/widgets/omdb_appbar_widget.dart';
 import 'package:omdbapp/core/widgets/widgets.dart';
 import 'package:omdbapp/features/home/presentation/pages/horizontal_list_movie_widget.dart';
 import 'package:omdbapp/features/search/presentation/pages/search_delegate.dart';
@@ -22,64 +23,69 @@ class _HomeScreenState extends State<HomeScreen> {
     'Categories': Icon(CupertinoIcons.chevron_down, size: 16),
   };
 
+  final ScrollController _bgScrollController = ScrollController();
+  final ValueNotifier<double> _isScrolled = ValueNotifier<double>(1);
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _bgScrollController.addListener(_scrollListener);
+    super.initState();
+  }
+
+  void _scrollListener() {
+    // Access the current scroll offset
+    double currentOffset = _bgScrollController.offset;
+    //   if currentOffset = 460 bg should be invisible
+    if (currentOffset < 460) {
+      final valueScrollConvert = 100 - (currentOffset * 100 / 460);
+      _isScrolled.value = valueScrollConvert / 100;
+    } else {
+      if (_isScrolled.value > 0) {
+        _isScrolled.value = 0;
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("For Alex"),
-        centerTitle: false,
-        backgroundColor: Color(0xFFD22F26),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Assets.svg.downloadIcon.svg(
-              colorFilter: ColorFilter.mode(ColorValue.white, BlendMode.srcIn),
-            ),
-          ),
-          IconButton(
-            onPressed: () async{
-              await showSearch(context: context, delegate: OmDbSearchDelegate());
-            },
-            icon: Assets.svg.searchIcon.svg(
-              colorFilter: ColorFilter.mode(ColorValue.white, BlendMode.srcIn),
-            ),
-          ),
-        ],
+      appBar: OmdbAppbarWidget(
+        chipIconAndText: chipIconAndText,
+        bodyScrollListener: _isScrolled,
       ),
       body: SingleChildScrollView(
+        controller: _bgScrollController,
         scrollDirection: Axis.vertical,
         child: SafeArea(
           child: Stack(
             children: [
               Positioned.fill(
-                child: Container(
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: [
-                        Color(0xFFD22F26),
-                        Color(0xFF8B1A1A),
-                        Color(0xFF1A0505),
-                        Color(0xFF000000),
-                      ],
-                      stops: [0.0, 0.3, 0.6, 1.0],
-                    ),
-                  ),
+                child: ValueListenableBuilder(
+                  valueListenable: _isScrolled,
+                  builder: (context, value, _) {
+                    print(value);
+                    return AnimatedContainer(
+                      duration: const Duration(milliseconds: 200),
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [
+                            Color(0xFFD22F26).withValues(alpha: value),
+                            Color(0xFF8B1A1A).withValues(alpha: value),
+                            Color(0xFF1A0505).withValues(alpha: value),
+                            Color(0xFF000000),
+                          ],
+                          stops: [0.0, 0.3, 0.5, 1.0],
+                        ),
+                      ),
+                    );
+                  },
                 ),
               ),
               Column(
                 children: [
-                  Align(
-                    alignment: Alignment.topLeft,
-                    child: Wrap(
-                      children: chipIconAndText.entries
-                          .map(
-                            (map) => OmdbChip(name: map.key, icon: map.value),
-                          )
-                          .toList(),
-                    ),
-                  ),
                   const SizedBox(height: 16),
                   MoviePromotion(),
                   const SizedBox(height: 16),
